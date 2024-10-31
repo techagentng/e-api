d531f51be580f332d9c73beb0dec20dc46d5a361
n.nnah@yahoo.com <techagentng@gmail.com>
2024-10-31 10:21:35 +0100

    add env configuration

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Debug                        bool   `envconfig:"debug"`
+	Port                         int    `envconfig:"port"`
+	PostgresHost                 string `envconfig:"postgres_host"`
+	PostgresUser                 string `envconfig:"postgres_user"`
+	PostgresDB                   string `envconfig:"postgres_db"`
+	BaseUrl                      string `envconfig:"base_url"`
+	Env                          string `envconfig:"env"`
+	PostgresPassword             string `envconfig:"postgres_password"`
+	JWTSecret                    string `envconfig:"jwt_secret"`
+	Host                         string `envconfig:"host"`
+	AccessControlAllowOrigin     string `envconfig:"accessc_control_allow_origin"`
+}
+
+func Load() (*Config, error) {
+	env := os.Getenv("GIN_MODE")
+	if env != "release" {
+		if err := godotenv.Load("./.env"); err != nil {
+			log.Printf("couldn't load env vars: %v", err)
+		}
+	}
+
+	c := &Config{}
+	err := envconfig.Process("ecomm", c)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
